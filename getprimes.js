dc761724657d54741ef50fdda092dc6bdf1a67b0
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-05-08 21:44:09 +0100

    Wrote test for invalid/negative/zero input cases

--- getprimes.js
+++ getprimes.js
@@ -1,24 +1,31 @@
-let primeNumbers=[];
-
-const getPrimes = (n)=>
+module.exports={
+getPrimes:(n)=>
 {
-	for(let element=0;element<=n;++element)
-	{
-		let halfOfElement = Math.floor(element/2);//reduced by half in order to reduce iterations
-		let count = 0;
+	let primeNumbers=[];
 
-		for(let divider=2;divider<=halfOfElement;++divider)
+	if (typeof n!==number)
+		return "This is not a number!"
+	else
 		{
-			if (element%divider===0)
-			{
-				++count;
-				break;
-			}
+			for(let element=0;element<=n;++element)
+				{
+					let halfOfElement = Math.floor(element/2);//reduced by half in order to reduce iterations
+					let count = 0;
+
+					for(let divider=2;divider<=halfOfElement;++divider)
+						{
+							if (element%divider===0)
+								{
+									++count;
+									break;
+								}
+						}
+
+					if (count===0)
+						primeNumbers.push(element);
+				}	
 		}
 
-		if (count===0)
-			primeNumbers.push(element);
-	}
-
 	return primeNumbers;
+}
 }
